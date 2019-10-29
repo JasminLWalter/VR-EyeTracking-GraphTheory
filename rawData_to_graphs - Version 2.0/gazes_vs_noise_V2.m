1e77c1974eaa500d79358fbc477b7b4eabe0505c
jasminwalter <45793671+jasminwalter@users.noreply.github.com>
2019-10-29 10:52:17 +0100

    updated gazes_vs_noise
    added new naming to make it more intuitive
    added new participant list

---
+++ rawData_to_graphs - Version 2.0/gazes_vs_noise_V2.m
@@ -0,0 +1,96 @@
+%% ------------------gazes_vs_noise----------------------------------------
+% script written by Jasmin Walter
+
+% divides condensed viewed houses list based on a set min sample value into 
+% objects that were gazed at and objects that were not gazed at (noisy
+% samples)
+% in other words: devides condensed viewed houses list into ganzed houses
+% and noise
+
+% uses condensedViewedHouses file as input
+% output: list with glanced houses per subject -> objects that were gazed at
+%         list with noisy sample objects per subject -> objects that were
+%         not gazed on
+%         overall overview with percentage of gazed/noisy object list
+
+
+clear all;
+
+savepath = 'E:\SeahavenEyeTrackingData\duringProcessOfCleaning\gazes_vs_noise\';
+
+cd 'E:\SeahavenEyeTrackingData\duringProcessOfCleaning\interpolateLostData\'
+
+% participant list of 90 min VR - only with participants who have lost less than 30% of
+% their data (after running script cleanParticipants_V2)
+PartList = {1909 3668 8466 2151 4502 7670 8258 3377 9364 6387 2179 4470 6971 5507 8834 5978 7399 9202 8551 1540 8041 3693 5696 3299 1582 6430 9176 5602 3856 7942 6594 4510 3949 3686 6543 7205 5582 9437 1155 8547 8261 3023 7021 9961 9017 2044 8195 4272 5346 8072 6398 3743 5253 9475 8954 8699 3593};
+
+Number = length(PartList);
+noFilePartList = [];
+countMissingPart = 0;
+countAnalysedPart= 0;
+
+overviewGazes = array2table(zeros(Number,4));
+overviewGazes.Properties.VariableNames = {'Participant','totalAmount','gazes','noise'};
+
+
+for ii = 1:Number
+    currentPart = cell2mat(PartList(ii));
+    
+    file = strcat(num2str(currentPart),'_interpolatedViewedHouses.mat');
+ 
+    % check for missing files
+    if exist(file)==0
+        countMissingPart = countMissingPart+1;
+        
+        noFilePartList = [noFilePartList;currentPart];
+        disp(strcat(file,' does not exist in folder'));
+    %% main code   
+    elseif exist(file)==2
+        countAnalysedPart = countAnalysedPart +1;
+        % load data
+        interpolatedData = load(file);
+        interpolatedData = interpolatedData.interpolatedData;
+        
+        % something was fixated when having more than 7 samples
+        
+
+        gazes = interpolatedData.Samples > 7;
+
+        
+        gazedObjects = interpolatedData(gazes,:);
+        
+        noisyObjects = interpolatedData(not(gazes),:);
+              
+        
+        
+        % save both tables
+        save([savepath 'gazes_' num2str(currentPart) '.mat'],'gazedObjects');
+        save([savepath 'noisy_data_' num2str(currentPart) '.mat'],'noisyObjects');
+        
+        % update overview with values
+        
+        overviewGazes.Participant(countAnalysedPart)= currentPart;
+        overviewGazes.totalAmount(countAnalysedPart)= height(interpolatedData);
+        overviewGazes.gazes(countAnalysedPart)= height(gazedObjects);
+        overviewGazes.noise(countAnalysedPart) = height(noisyObjects);
+        
+     
+  
+
+        
+    else
+        disp('something went really wrong with participant list');
+    end
+
+end
+disp(strcat(num2str(Number), ' Participants in List'));
+disp(strcat(num2str(countAnalysedPart), ' Participants analyzed'));
+disp(strcat(num2str(countMissingPart),' files were missing'));
+
+csvwrite(strcat(savepath,'Missing_Participant_Files'),noFilePartList);
+disp('saved missing participant file list');
+
+save([savepath 'Overview_Gazes.mat'],'overviewGazes');
+disp('saved Overview Gazes');
+
+disp('done');
