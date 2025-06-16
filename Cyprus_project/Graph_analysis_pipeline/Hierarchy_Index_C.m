005dd7de84b192a96615b68a7e4bac5b919bc6eb
Jasmin <jawalter@uos.de>
2025-06-16 20:04:36 +0100

    upload analysis scripts Cyprus project

---
+++ Cyprus_project/Graph_analysis_pipeline/Hierarchy_Index_C.m
@@ -0,0 +1,164 @@
+%% -------------------- Hierarchy_Index -----------------------
+
+% -------------------- written by Lucas Essmann - 2020 --------------------
+% ---------------------- lessmann@uni-osnabrueck.de -----------------------
+
+%-----------------adjusted and extended by Jasmin L. Walter, 2022/23-------
+%---------------------------jawalter@uos.de--------------------------------
+
+%Hierarchy index 
+
+% Requirements:
+% undirected, unweighted graphs with Edges and Nodes Table 
+
+%--------------------------------------------------------------------------
+%Procedure: 
+%Plotting all occuring degree values against their frequency
+%Afterwards fitting a power law function (a*x^b). 
+%The inverse of b is the Hierarchy Index, the higher the index, the
+%stronger the hierarchy. Values below 1 are considered as no existing
+%hierarchy.
+
+clear all;
+
+plotting_wanted = true; %if you want to plot, set to true
+saving_wanted = true; %if you want to save, set to true
+
+
+%% -------------------------- Initialisation ------------------------------
+path = what;
+path = path.path;
+
+%savepaths
+savepath = 'E:\Cyprus_project_overview\data\analysis\exploration\eyeTracking\graph-analysis-pipeline\HierarchyIndex\';
+
+% cd into graph folder location
+cd 'E:\Cyprus_project_overview\data\analysis\exploration\graphs\';
+
+
+totalgraphs = 1;
+% amount of graphs
+HierarchyIndex = table();
+
+    
+%% ----------------------------- Main -------------------------------------
+
+    currentPart = '1';
+    partIndex= 1;
+    
+    %Create and reset the variables in the loop
+    NodeDegree = [];
+    UniqueDegree = [];
+    NodeDegreeMed = [];
+    UniqueDegreeMed = [];
+    DegreeFrequency = [];
+    DegreeFrequencyMed = [];
+
+   %load graph
+    graphy = load('graph_limassol.mat');
+    graphy = graphy.graphy;
+
+
+   %Calculate the degree
+    NodeDegree = degree(graphy); 
+    
+%% ------------------------- Hierarchy Index ------------------------------
+   %Delete 0 degrees (non connected nodes)
+    NodeDegree(NodeDegree == 0) = [];
+    UniqueDegree = unique(NodeDegree);
+    UniqueDegree = sort(UniqueDegree);
+   %do another fit only for the NDs > median(NodeDegree) 
+    NodeDegreeMed = NodeDegree(NodeDegree>median(NodeDegree));
+    UniqueDegreeMed = unique(NodeDegreeMed);
+    UniqueDegreeMed = sort(UniqueDegreeMed);
+    
+    
+   %Count how often each degree exists for all nodes (Degree Frequency)
+    for ndfreq = 1:length(UniqueDegree)
+        DegreeFrequency(ndfreq,:) = ...
+            sum(NodeDegree(:) == UniqueDegree(ndfreq,1));
+    end
+    
+   %Same for the meds
+    for medfreq = 1:length(UniqueDegreeMed)
+        DegreeFrequencyMed(medfreq,:) = ...
+            sum(NodeDegreeMed(:) == UniqueDegreeMed(medfreq,1));
+    end
+    
+   %log the axis
+    UniqueDegreeMed_log = log(UniqueDegreeMed);
+    DegreeFrequencyMed = log(DegreeFrequencyMed);
+   %Fit a curve to the log data above the median with an exponential fit
+   %(ax+b)
+    ft2 = fittype(@(a,b,x) a*x+b);
+    f2 = fit(UniqueDegreeMed_log,DegreeFrequencyMed,ft2);
+    HierarchyIndex.Part(partIndex,:) = currentPart;
+    HierarchyIndex.Slope(partIndex,:) = f2.a;
+    
+   %Fit a curve to the data with a Power Fit
+    ft = fittype(@(a,b,x)a*x.^b);
+    f = fit(UniqueDegree,DegreeFrequency,ft);
+   %The hierarchy index is the inverse exponent of x
+    HierarchyIndex.HierarchyIndex(partIndex,:) = -f.b;
+
+    
+%% -------------------------- Plotting ------------------------------------
+
+    if plotting_wanted == true
+    
+        figgy = figure();
+        scatter(log(UniqueDegree),...
+            log(DegreeFrequency),...
+            300,...
+            [0.24,0.15,0.66],...
+            'filled');
+        hold on;
+        plotty = fplot(@(x) f2.a*x+f2.b,...
+            [min(UniqueDegreeMed_log),max(UniqueDegreeMed_log)],...
+            'LineWidth',3,'Color',[0.96,0.73,0.23]);
+        xlim([0 4]); ylim([0 4]);
+        xticks([0,1,2,3,4]);
+        yticks([0,1,2,3,4]) 
+        rectangle('Position',[0 0 4 4],'LineWidth',1.5);
+
+
+%         plotty2 = plot(f2,...
+%             log(UniqueDegree),...
+%             log(DegreeFrequency),...
+%             [min(DegreeFrequencyMed),max(DegreeFrequencyMed)]);
+%         xlabel('Degree'); ylabel('Frequency');
+%         set(gca,'FontName','Helvetica','FontSize',40,'FontWeight','bold')
+        
+    if saving_wanted == true
+         saveas(gcf,strcat(savepath,...
+             'Med_Hierarchy_Fit_Part',...
+             num2str(currentPart),'.png'),'png');
+    end
+
+         close(figgy);   
+   
+    end
+     
+
+
+histy = histogram(HierarchyIndex.Slope,7);
+xlabel('Hierarchy Index'); 
+ylabel('Frequency');
+%     ylim([0 6.5]);
+set(gca,'FontName','Helvetica')%,'FontSize',40)
+saveas(gcf,strcat(savepath,...
+             'Histogram_HierarchyIndex.png'),'png');
+         
+%% --------------------------- Saving -------------------------------------
+
+if saving_wanted == true
+    
+    save([savepath 'HierarchyIndex_Table.mat'],'HierarchyIndex');
+    disp('Saved HierarchyIndex_Table');
+    
+end
+
+disp('Done');
+
+% clearvars '-except' ...
+%     HierarchyIndex;
--- Cyprus_project/extract_building_coordinate_list.m
+++ Cyprus_project/extract_building_coordinate_list.m
@@ -9,6 +9,7 @@ cd 'E:\Cyprus_project_overview\data\buildings\'
 
 buildingCoordinates = readtable('Final Map Limassol City Center (numbered buildings)- Building Labels.csv');
 
+imagepath = 'E:\Cyprus_project_overview\data\maps\'; % path to the map image location
 
 
 % Example of table data:
@@ -42,14 +43,197 @@ buildingCoordinates.Latitude = latitude;
 buildingCoordinates.buildingNames = extractedNumbers;
 
 
-writetable(buildingCoordinates, 'building_coordinate_list.csv');
+colliderList = buildingCoordinates; % save unedited version for later analysis and plotting
+
+
+%% check for duplications (in building names and coordinate locations)
+
+
+% Identify duplicate rows based on all columns
+[~, idx] = unique(buildingCoordinates.buildingNames); 
+duplicated_rows = setdiff(1:height(buildingCoordinates.buildingNames), idx);
+
+
+duplicated_data = buildingCoordinates(duplicated_rows, :);
+
+isDupl = ismember(buildingCoordinates.buildingNames, duplicated_data.buildingNames);
+
+allDuplicates_name = buildingCoordinates(isDupl, :);
+
+% to handle the duplicates, rename them
+
+
+% Handle the duplicates by renaming them
+for i = 1:height(duplicated_data)
+    % Get the name of the current duplicated building
+    current_name = duplicated_data.buildingNames{i};
+    
+    % Find all occurrences of this name
+    duplicate_indices = find(strcmp(buildingCoordinates.buildingNames, current_name));
+    
+    % Skip the first occurrence and rename the rest
+    
+    buildingCoordinates.buildingNames{duplicate_indices(2)} = char(string(str2num(current_name) + 500));
+
+
+end
+
+    
+% now check for duplicates in coordinates
+
+[~, idx2] = unique(buildingCoordinates(:,5:6),'rows');
+duplicated_rows2 = setdiff(1:height(buildingCoordinates.buildingNames), idx2);
+
+duplicated_data2 = buildingCoordinates(duplicated_rows2, :);
+
+isDupl2 = (buildingCoordinates.Latitude == duplicated_data2.Latitude) & (buildingCoordinates.Longitude == duplicated_data2.Longitude);
+
+allDuplicates_coord = buildingCoordinates(isDupl2, :);
+
+% there is one duplicate, but this is already handled in the data frame.
+% Although 2 identical locations are here marked with 2 names (77 & 76),
+% only 77 is in the graph
+
+
+% writetable(buildingCoordinates, 'building_coordinate_list.csv');
+
+
+%% plot duplicates on map to investigate them (optional)
+
+map = imread(strcat(imagepath,'map2.jpg'));
+
+
+% pixel values map picture  8192x5051
+dimMap = size(map);
+pixel_width = dimMap(2);  % Width of the image in pixels
+pixel_height = dimMap(1);   % Height of the image in pixels
+
+
+% big map --> map 2 data
+% latitude and longitude of the edges of the map image 
+corner_lat_top =  34.677833; % Latitude of the top edge of the map
+corner_lat_bottom = 34.6714944;  % Latitude of the bottom edge of the map
+corner_lon_left = 33.0378529;  % Longitude of the left edge of the map
+corner_lon_right = 33.0496599; % Longitude of the right edge of the map
+
+
+% Calculate differences in latitude and longitude
+delta_lat = corner_lat_top - corner_lat_bottom;
+delta_lon = corner_lon_right - corner_lon_left;
+
+% Calculate conversion ratio from degrees to pixels
+lat_to_pixel = pixel_height / delta_lat;
+lon_to_pixel = pixel_width / delta_lon;
+
+% Assuming df contains 'latitude' and 'longitude' columns
+% Convert GPS coordinates to pixel coordinates
+colliderList.Longitude= (colliderList.Longitude - corner_lon_left) * lon_to_pixel;
+colliderList.Latitude = (corner_lat_top - colliderList.Latitude) * lat_to_pixel;
+
+% display map
+figure(1)
+imshow(map);
+alpha(0.7)
+hold on;
+
+
+ % plot visualization
+%          plotty = scatter(x,y,40,'filled','YDataMode','manual');
+
+hold on
+
+x = colliderList.Longitude(isDupl);
+y = colliderList.Latitude(isDupl);
+
+plotty = scatter(x, y, 15 ,'b','filled');
+
+saveas(gcf,strcat('All duplicated building locations.png'));
+
+
+
+% x = colliderList.Longitude(duplicated_rows);
+% y = colliderList.Latitude(duplicated_rows);
+% 
+% plotty = scatter(x, y, 12 ,'r','filled');
+
+hold off
+
+%% plot the individual duplicated nodes with their edges --> only do when you have the graph already
+
+% load graph
+
+graphy = load('E:\Cyprus_project_overview\data\analysis\exploration\graphs\graph_limassol.mat');
+graphy= graphy.graphy;
+
+edgeCell = graphy.Edges.EndNodes;
+
+
+for index = 1:height(duplicated_data)
+
+    duplName = duplicated_data.buildingNames(index);
+    duplLocs = strcmp(colliderList.buildingNames, duplName);  
+
+    e1 = strcmp(edgeCell(:,1), duplName);
+    e2 = strcmp(edgeCell(:,2), duplName);
+
+    edges = edgeCell(e1|e2,:);
+
+    % display map
+    figure;
+    imshow(map);
+    alpha(0.7)
+    hold on;
+
+
+    for ee = 1:length(edges)
+
+        if not(strcmp(edges(ee,1), duplName))
+
+            loc = strcmp(colliderList.buildingNames,edges(ee,1));
+    
+            x = colliderList.Longitude(loc);
+            y = colliderList.Latitude(loc);
+        
+            plotty = scatter(x, y, 15 ,'r','filled');
+
+        end
+
+
+        % [Xhouse,Xindex] = ismember(edges(ee,1), colliderList.buildingNames);
+        % 
+        % [Yhouse,Yindex] = ismember(edges(ee,2), colliderList.buildingNames);
+        % 
+        % x1 = colliderList.Longitude(Xindex);
+        % y1 = colliderList.Latitude(Xindex);
+        % 
+        % x2 = colliderList.Longitude(Yindex);
+        % y2 = colliderList.Latitude(Yindex);
+        % 
+        % line([x1,x2],[y1,y2],'Color','k','LineWidth',1); 
+    
+    end
+    
+    
+    x = colliderList.Longitude(duplLocs);
+    y = colliderList.Latitude(duplLocs);
+    
+    plotty = scatter(x, y, 15 ,'b','filled');
+    
+    
+    % x = colliderList.Longitude(duplicated_rows);
+    % y = colliderList.Latitude(duplicated_rows);
+    % 
+    % plotty = scatter(x, y, 12 ,'r','filled');
+
+    title(strcat(duplName,' building duplicate + edge buildings'))
+    
+    hold off
+    saveas(gcf,strcat(string(duplName),'_duplicated_building_and_edgeBuildings.png'));
 
 
 
 
-
-
-
+end
 
 
 
