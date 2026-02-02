c155111c5432053a0d50c3e7ddcaaf1c23c0be62
Jasmin <jawalter@uos.de>
2026-02-03 00:39:58 +0100

    add version without text

--- GraphTheory_ET_VR_Westbrueck/Analysis/modify_map_WB.m
+++ GraphTheory_ET_VR_Westbrueck/Analysis/modify_map_WB.m
@@ -5,11 +5,11 @@
 clear all;
 
 
-imagepath = 'D:\Github\NBP-VR-Eyetracking\GraphTheory_ET_VR_Westbrueck\additional_Files\'; % path to the map image location
+imagepath = 'D:\Github\VR-EyeTracking-GraphTheory\GraphTheory_ET_VR_Westbrueck\additional_Files\'; % path to the map image location
 
 
 
-cd 'D:\Github\NBP-VR-Eyetracking\GraphTheory_ET_VR_Westbrueck\additional_Files\'
+cd 'D:\Github\VR-EyeTracking-GraphTheory\GraphTheory_ET_VR_Westbrueck\additional_Files\'
 
 figure(1)
 map = imread (strcat(imagepath,'map_natural_white.png'));
@@ -36,7 +36,9 @@ line([m100*2,m100*3],[yHeight2,yHeight2],'Color','w','LineWidth',5);
 line([m100*3,m100*4],[yHeight2,yHeight2],'Color','k','LineWidth',5);   
 line([m100*4,m100*5],[yHeight2,yHeight2],'Color','w','LineWidth',5); 
 
-text(m100*5 + 50,yHeight2-25,'500 m','FontSize',8, 'FontName', 'Helvetica');
+% text(m100*5 + 50,yHeight2-25,'500 m','FontSize',8, 'FontName', 'Helvetica');
+
+
 % find height of the map with markers
 % scatter(columns,476,'r')
 % scatter(columns,476.5,'b')
@@ -55,15 +57,15 @@ disp(width)
 disp('height of map')
 disp(height)
 
-text(100,3770,{'Map width =  ','Map height =  '},'FontSize',8, 'FontName', 'Helvetica');
-text(700,3770,{strcat(num2str(width),'m'),strcat(num2str(height),'m')},'FontSize',8, 'FontName', 'Helvetica');
-
+% text(100,3770,{'Map width =  ','Map height =  '},'FontSize',8, 'FontName', 'Helvetica');
+% text(700,3770,{strcat(num2str(width),'m'),strcat(num2str(height),'m')},'FontSize',8, 'FontName', 'Helvetica');
+% 
 
 
 
 ax = gca;
-exportgraphics(ax,strcat(imagepath,'map_natural_500mMarker_600dpi.png'),'Resolution',600)
-exportgraphics(ax,strcat(imagepath,'map_natural_500mMarker.png'))
+exportgraphics(ax,strcat(imagepath,'map_natural_500mMarker_600dpi_withoutText.png'),'Resolution',600)
+exportgraphics(ax,strcat(imagepath,'map_natural_500mMarker_withoutText.png'))
 
 
 hold off
